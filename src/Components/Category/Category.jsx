87cbf4dedff974b0e55e847df2513b6af977e70e
Isaac <isaac.ruiz0219@gmail.com>
2022-08-06 19:18:25 -0700

    set mobile styling for category section

--- src/Components/Category/Category.jsx
+++ src/Components/Category/Category.jsx
@@ -2,11 +2,23 @@ import React from 'react'
 import './category.scss'
 import {useNavigate} from 'react-router-dom';
 import { motion } from 'framer-motion';
+import { MdMenuBook } from "react-icons/md";
+import { FaMountain } from 'react-icons/fa';
+import { BsClockHistory } from 'react-icons/bs';
+import { MdOutlineScience } from 'react-icons/md';
+import { BsMusicNoteList } from 'react-icons/bs';
+import { FaFilm } from 'react-icons/fa';
 
 function Category() {
   const navigate = useNavigate()
 
-  const categories = [{category: 'General Knowledge'},{category: 'Geography'},{category: 'History'},{category: 'Science'},{category: 'Music'},{category: 'Film'}]
+  const categories = [
+    {category: 'General Knowledge', icon: MdMenuBook},
+    {category: 'Geography', icon: FaMountain},
+    {category: 'History', icon: BsClockHistory},
+    {category: 'Science', icon: MdOutlineScience},
+    {category: 'Music', icon: BsMusicNoteList},
+    {category: 'Film and Tv', icon: FaFilm}]
   return (
     <motion.div
      id='category'  
@@ -18,9 +30,13 @@ function Category() {
         <h1 onClick={()=>{navigate('/')}}>Choose Category</h1>
       </div>
       <main>
+
         {categories.map((categoryObject, key)=>{
+          const Icon = categoryObject.icon
+
           return (
             <div className="categoryChoice">
+              <Icon className='categoryIcons'/>
               <h3>{categoryObject.category}</h3>
             </div>
           )
