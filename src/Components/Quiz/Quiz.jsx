ba964f3ca3ae3c845808beaa231de4dc2ebdf684
Isaac <isaac.ruiz0219@gmail.com>
2022-08-08 21:42:54 -0700

    Finally fixed the bug so now the questions are dynamic and display answers in a shuffled order for the user to answer

--- src/Components/Quiz/Quiz.jsx
+++ src/Components/Quiz/Quiz.jsx
@@ -9,41 +9,97 @@ import {Helmet} from "react-helmet";
 function Quiz() {
     const location = useLocation()
     const navigate = useNavigate()
-    // These values were passed by the difficulty Component
-    const categoryName = location.state?.name
-    const difficulty = location.state?.difficulty
     const titleName = location.state?.originalName
+    const [questionNumber, setQuestionNumber] = useState(1)
+
     // These states will display the data of the question
     const [question,setQuestion] = useState('')
     const [shuffledArray, setShuffledArray] = useState([])
+    // This will contain the array of objects regarding the question data
+    const [questionArray, setQuestionArray] = useState([])
+    // This will display what number the user is on
+    const [numberUserIsOn, setNumberUserIsOn] =useState(1)
 
+
+
+    // const categoryName = location.state?.name
+    // const difficulty = location.state?.difficulty
 
     const getQuiz = async () => {
         try{
+            // These values were passed by the difficulty Component
+            const categoryName = location.state?.name
+            const difficulty = location.state?.difficulty
+
+            // This makes a get request to get data for the quiz
             let response = await axios.get(`https://the-trivia-api.com/api/questions?categories=${categoryName}&limit=10&difficulty=${difficulty}`)
-            setQuestion(response.data[0].question)
-            const possibleAnswers = []
-            // These two pushes will add the correct answer to the possibleAnswers array
-            possibleAnswers.push(response.data[0].correctAnswer)
-            response.data[0].incorrectAnswers.forEach(answer => {
-               possibleAnswers.push(answer)  
-            });
-            // this will shuffle the array so that the answer is at a different index
-            const sortAndShuffleArray = possibleAnswers.sort(() => {
-                return Math.random() - 0.5
-            })
-    
-            setShuffledArray(sortAndShuffleArray)
+            let arrayDataResponse = await response.data
+
+            // This sets the data to question array so that it is accessible outside of this function
+            setQuestionArray(arrayDataResponse)
+
+            // this outputs an empty array
+            console.log(questionArray)
+
         } catch(err){
             console.log(err)
         }
 
     }
-    useEffect(()=>{
-        window.scrollTo(0, 0)
-        getQuiz()
 
-      },[])
+    // This fetches the data on mount
+    useEffect(() => { getQuiz() }, [])
+
+    // This will set the data for the elements once the state of the question array has been set from the get request
+    useEffect(() => { 
+
+        if (questionArray[0]) { 
+            setQuestion(questionArray[0].question) 
+            console.log(questionArray)
+
+            // This array will take in the incorrect answers and correct answers
+            let unshuffledArray = []
+
+            // This will push all possible answers to the unshuffled array 
+            questionArray[0].incorrectAnswers.forEach(incorrectAnswer => unshuffledArray.push(incorrectAnswer))
+            unshuffledArray.push(questionArray[0].correctAnswer)
+            // We will shuffle the inncorrect and correct answers and then display the choces to the user
+            let shuffledArray = unshuffledArray.sort(() => (Math.random() > .5) ? 1 : -1)
+
+            // This will set the unshuffled array to the be displayed 
+            setShuffledArray(shuffledArray)
+
+            console.log(unshuffledArray)
+        }
+
+    }, [questionArray])
+
+    const handleNextQuestion = () =>{
+
+        setNumberUserIsOn(numberUserIsOn + 1)   
+        if (numberUserIsOn > 10){
+            // This will take them to page where they can see how well they did
+            
+        }
+        else{
+            setQuestion(questionArray[questionNumber].question)
+            // This array will take in the incorrect answers and correct answers
+            let unshuffledArray = []
+    
+            // This will push all possible answers to the unshuffled array 
+            questionArray[questionNumber].incorrectAnswers.forEach(incorrectAnswer => unshuffledArray.push(incorrectAnswer))
+            unshuffledArray.push(questionArray[questionNumber].correctAnswer)
+            // We will shuffle the inncorrect and correct answers and then display the choces to the user
+            let shuffledArray = unshuffledArray.sort(() => (Math.random() > .5) ? 1 : -1)
+    
+            // This will set the unshuffled array to the be displayed 
+            setShuffledArray(shuffledArray)
+            
+            setQuestionNumber(questionNumber + 1)
+         }
+
+    }
+
     return (
         <motion.div
         id='quiz'
@@ -57,7 +113,7 @@ function Quiz() {
             </Helmet>
             <div className="title">
                 <h5>{titleName}</h5>
-                <h5></h5>
+                <h5>{numberUserIsOn}/10</h5>
             </div>
             <header className="questionDiv">
                 <h4>{question}</h4>
@@ -65,7 +121,7 @@ function Quiz() {
             <main>
                 {shuffledArray.map((possibleAnswer, index)=>{
                     return (
-                        <div className="possibleAnswerDiv" key={index}>
+                        <div className="possibleAnswerDiv" key={index} onClick={handleNextQuestion} >
                             <h3>
                                 {possibleAnswer}
                             </h3>
